c704147c220c9d36d406fe3f6a644a69615e0cb6
Hyeungshik Jung <hyungshike@gmail.com>
2017-05-04 15:21:01 +0900

    Add UIST 2017 demo deadline

--- _events.js
+++ _events.js
@@ -1,5 +1,9 @@
 const items = [
     {
+      "title": "UIST 2017 Demo",
+      "time": "July 12, 2017 12:00 PDT"
+    },
+    {
         "title": "CHI 2018 Full Paper",
         "time": "September 14, 2017 10:00 PDT"
     }
