d4a2fbaee32db0125a7367d57f429ac9ccace7d3
Paul Grau <paul@graycoding.com>
2017-10-12 15:18:54 +0900

    fix dates

--- events.js
+++ events.js
@@ -1,10 +1,10 @@
 var EVENTS = [
     {
         "title": "L@S 2018 Full Paper",
-        "time": "January 21, 2017 12:00 PDT"
+        "time": "January 21, 2018 12:00 PDT"
     },
     {
         "title": "L@S 2018 WIP",
-        "time": "March 24, 2017 12:00 PDT"
+        "time": "March 24, 2018 12:00 PDT"
     }
 ]
